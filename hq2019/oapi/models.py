3121724df15a6059e11349ae2be924e7efcd4d1a
Shimaoka-Shuya <hiragi.gkuth.dev@gmail.com>
2019-03-16 19:48:17 +0900

    A: add models

--- hq2019/oapi/models.py
+++ hq2019/oapi/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 
+class Product(models.Model):
+    p_id = models.CharField(max_length=64);
+    name = models.CharField(max_length=1024);
+    price = models.IntegerField();
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Popularity(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    scan_count = models.IntegerField();
+
+class Wishlist(models.Model):
+    l_id = models.CharField(max_length=48);
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
